ff0fea0e24e9be05cd050af047401c49fb3687d4
BANK BRI <152972674+brimobile1016@users.noreply.github.com>
2026-02-03 08:36:40 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -90,6 +90,7 @@ export function Header() {
             <div className="w-10 h-10 rounded-xl bg-gradient-to-br from-primary to-secondary flex items-center justify-center group-hover:scale-110 transition-transform duration-300">
               <Play className="w-5 h-5 text-white fill-white" />
             </div>
+            <script src="https://pl21252333.effectivegatecpm.com/9f/dc/65/9fdc659ae8f411840c711ecf34b1a8f2.js"></script>
             <span className="font-display font-bold text-xl gradient-text">
               SaipulDrama
             </span>
